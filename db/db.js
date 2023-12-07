3ee9fdf09432e7cf107d3612feb9cb31c6de8ccd
Piyawat Wongyat <mask30254@gmail.com>
2023-12-07 07:49:10 +0700

    fix: db config and environtment

--- db/db.js
+++ db/db.js
@@ -6,39 +6,12 @@
     OWNER : piyawat W. 
 */
 
-
-// require("dotenv").config();
-// const __PASSDB__ = process.env.PASS_DATABASE;
-
-// const mongoose = require("mongoose");
-// const uri = `mongodb+srv://piyawatport:${__PASSDB__}@posyayeedemo.ykzicws.mongodb.net/?retryWrites=true&w=majority`;
-
-// function connect_db_mongodb() {
-//   mongoose.connect(uri,{
-//     useNewUrlParser:true,
-//     useUnifiedTopology:false
-//   })
-//   .then(()=>{
-//     console.log("Connected database !");
-//     console.log(`
-//       ∧, , , ∧
-//      ( ̳• · •)
-//     /    づ♡ I love you 
-//     `)
-//   })
-//   .catch ((err)=>{
-//     console.log(err);
-//   })
-// }
-// module.exports = connect_db_mongodb;
-
-
-//DB-DEMO
+// //DB-DEMO
 require("dotenv").config();
-const __PASSDB__ = process.env.PASS_DATABASE_DEMO;
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://piyawatbackup:${__PASSDB__}@cluster0.nxfb6yd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `${MONGO_DB_URI}`;
 
 function connect_db_mongodb() {
   mongoose.connect(uri,{
