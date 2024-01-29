89a87d54883279aea5a4daa30fc83ffab797b939
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-01-29 11:57:06 +0700

    refactor: new login using cookies

--- auth/con_login.js
+++ auth/con_login.js
@@ -21,11 +21,31 @@ exports.con_login = async (req, res) => {
     //ถ้ารหัสไม่ตรง
     if (!isPasswordValid) {
       return res.status(401).json({ error: "รหัสผ่านไม่ถูกต้อง" });
-    } 
+    }
 
     //ถ้าพาสตรงยูสตรง
-    const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'1d'});
-    return res.json({token,username});
+    const userId = user?.id;
+    const accessToken = jwt.sign({ username, userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    const refreshToken = jwt.sign({ username, userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    // Calculate the expiration date for 30 days from now
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 30);
+
+    //Send cookies
+    res.cookie('AccessToken', accessToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+      secure: true,
+      httpOnly: true,
+      sameSite: 'None',
+      expires: expirationDate
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "ล็อกอินสำเร็จค่ะ!",
+      refresh: refreshToken
+    });
 
   } catch (error) {
     console.error(error);
